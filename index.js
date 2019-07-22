438afd077392eb74e21c0262eadf6b6d748a7161
chikku123 <53073211+chikku123@users.noreply.github.com>
2019-07-22 16:53:58 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,25 +1,66 @@
 var express=require("express");
 var bodyParser=require('body-parser');
-const path = require('path');
- 
 var connection = require('./config');
 var app = express();
- 
+const path = require('path'); 
+const session = require('express-session');
+
 var authenticateController=require('./controllers/authenticate-controller');
 var registerController=require('./controllers/register-controller');
  
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'app-assets'))); 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.get('/', function (req, res) {  
-   res.sendFile( __dirname + "/" + "login.html" );  
+app.use(express.static(path.join(__dirname,'public')));
+app.use(session({secret: 'ssshhhhh',saveUninitialized: true,resave: false}));
+var sess;
+
+app.get('/', function (req, res) { 
+
+   sess = req.session; 
+   if(sess.userid) {
+      res.sendFile( __dirname + "/" + "dashboard.html" );  
+
+	}
+   res.sendFile( __dirname + "/" + "login.html" ); 
+  // res.sendFile( __dirname + "/" + "login.html" );  
 })  
  
-app.get('/login.html', function (req, res) {  
+app.get('/dashboard', function (req, res) { 
+
+   sess = req.session; 
+   if(sess.userid) {
+      res.sendFile( __dirname + "/" + "dashboard.html" );  
+
+	}
+   res.sendFile( __dirname + "/" + "login.html" ); 
+  // res.sendFile( __dirname + "/" + "login.html" );  
+})
+
+
+app.get('/login.html', function (req, res) { 
+   sess = req.session; 
+   if(sess.userid) {
+      
+      res.sendFile( __dirname + "/" + "dashboard.html" );  
+
+	}
    res.sendFile( __dirname + "/" + "login.html" );  
 })  
- 
+app.get('/logout',(req,res) => {
+	req.session.destroy((err) => {
+		if(err) {
+			return console.log(err);
+		}
+		res.redirect('/');
+	});
+
+})
+
+
+
+
+
 /* route to handle login and registration */
 app.post('/api/register',registerController.register);
 app.post('/api/authenticate',authenticateController.authenticate);
